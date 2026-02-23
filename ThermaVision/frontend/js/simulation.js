e805418e64dc1139017e99e611762d0cb932c2b1
KGFCH2 <babinbid05@gmail.com>
2026-02-23 10:40:05 +0530

    Change API base to deployed URL in simulation

--- ThermaVision/frontend/js/simulation.js
+++ ThermaVision/frontend/js/simulation.js
@@ -5,7 +5,7 @@
 (function () {
     'use strict';
 
-    const API_BASE = 'http://127.0.0.1:8080';
+    const API_BASE = 'https://thermavision.onrender.com';
 
     const form = document.getElementById('simulation-form');
     const btnAnalyze = document.getElementById('btn-analyze');
